fbe61d56567da823e51be38d542db7c0f91dd323
path71nder <sevillebayuaji@gmail.com>
2019-09-25 15:08:15 +0700

    Bab 5 - Fragment : Embedded Fragment

---
+++ app/src/main/java/polinema/ac/id/starterchapter05/activities/StaticActivity.java
@@ -0,0 +1,19 @@
+package polinema.ac.id.starterchapter05.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import polinema.ac.id.starterchapter05.R;
+
+import polinema.ac.id.starterchapter05.MainActivity;
+
+public class StaticActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_static);
+
+    }
+}
