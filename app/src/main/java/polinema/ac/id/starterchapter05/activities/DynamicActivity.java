8ea4dde85ac55605d3755728a0c9c3bdba708d71
path71nder <sevillebayuaji@gmail.com>
2019-09-25 16:47:03 +0700

    Bab 5 - Fragment

--- app/src/main/java/polinema/ac/id/starterchapter05/activities/DynamicActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/activities/DynamicActivity.java
@@ -1,11 +1,14 @@
 package polinema.ac.id.starterchapter05.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
 
 import polinema.ac.id.starterchapter05.R;
+import polinema.ac.id.starterchapter05.fragment.BlueFragment;
+import polinema.ac.id.starterchapter05.fragment.RedFragment;
 
 public class DynamicActivity extends AppCompatActivity {
 
@@ -16,10 +19,14 @@ public class DynamicActivity extends AppCompatActivity {
     }
 
     public void handlerClickLoadRedFragment(View view) {
-
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.dynamic_fragment_placeholder,new RedFragment());
+        fragmentTransaction.commit();
     }
 
     public void handlerClickLoadBlueFragment(View view) {
-
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.dynamic_fragment_placeholder,new BlueFragment());
+        fragmentTransaction.commit();
     }
 }
