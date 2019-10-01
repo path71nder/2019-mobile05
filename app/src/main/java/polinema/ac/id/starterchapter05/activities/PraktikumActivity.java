9351867cef3b00ef7a79af2505385f32970f635b
path71nder <sevillebayuaji@gmail.com>
2019-10-01 18:57:39 +0700

    Bab 5 - Fragment (Tugas)

--- app/src/main/java/polinema/ac/id/starterchapter05/activities/PraktikumActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/activities/PraktikumActivity.java
@@ -1,11 +1,15 @@
 package polinema.ac.id.starterchapter05.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
 
 import polinema.ac.id.starterchapter05.R;
+import polinema.ac.id.starterchapter05.fragment.DipsFragment;
+import polinema.ac.id.starterchapter05.fragment.HandstandFragment;
+import polinema.ac.id.starterchapter05.fragment.PushUpFragment;
 
 public class PraktikumActivity extends AppCompatActivity {
 
@@ -16,11 +20,26 @@ public class PraktikumActivity extends AppCompatActivity {
     }
 
     public void handlerClickLoadPushUpFragment(View view) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        fragmentTransaction.replace(R.id.dynamic_fragment_placeholder,new PushUpFragment());
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 
     public void handlerClickLoadDipsFragment(View view) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        fragmentTransaction.replace(R.id.dynamic_fragment_placeholder,new DipsFragment());
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 
     public void handlerClickLoadHandstandFragment(View view) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        fragmentTransaction.replace(R.id.dynamic_fragment_placeholder,new HandstandFragment());
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 }
