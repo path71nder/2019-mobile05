b41be84b5e5f2233de88eb39a34d9cf05e55c1ac
path71nder <sevillebayuaji@gmail.com>
2019-10-01 07:31:36 +0700

    Bab 5 - Fragment (Tugas)

---
+++ app/src/main/java/polinema/ac/id/starterchapter05/activities/PraktikumActivity.java
@@ -0,0 +1,26 @@
+package polinema.ac.id.starterchapter05.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import polinema.ac.id.starterchapter05.R;
+
+public class PraktikumActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_praktikum);
+    }
+
+    public void handlerClickLoadPushUpFragment(View view) {
+    }
+
+    public void handlerClickLoadDipsFragment(View view) {
+    }
+
+    public void handlerClickLoadHandstandFragment(View view) {
+    }
+}
